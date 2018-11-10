f0e5b38cbbdee8b37ca8cc243f603c1d5380cc1a
albertpai <verdant.light@gmail.com>
2018-11-10 23:19:05 +0800

    Exercise 4: Check Speed

---
+++ exercise4_checkSpeed.js
@@ -0,0 +1,29 @@
+/*
+1. Print out 'Ok' if speed is lower than speed limit plus extra speed per point.
+2. Print out points if speed is higher than speed limit.
+3. Print out 'Licence suspended' if points are more than 12.
+4. Test different numbers around the borderlines: 
+*/
+
+function checkSpeed(speed) {
+    const speedLimit = 70;
+    const kmPerPoint = 5;
+    const points = Math.floor((speed-speedLimit)/kmPerPoint);
+    
+    if (speed < speedLimit + kmPerPoint) {
+        console.log('Ok');
+        return;
+    }
+    if (points >= 12)
+    console.log('Licence suspended');
+    else
+    console.log(`Points: ${points}`); 
+}
+
+checkSpeed(70);
+checkSpeed(74.9);
+checkSpeed(75);
+checkSpeed(100);
+checkSpeed(129);
+checkSpeed(129.9);
+checkSpeed(130);
